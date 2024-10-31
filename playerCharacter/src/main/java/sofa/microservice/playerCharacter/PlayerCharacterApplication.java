41ea34345decd9d0c2c7d3bfde8b1666fd57b686
kyppen <fagerhus.sondre@gmail.com>
2024-10-31 09:29:26 +0100

    added character module

---
+++ playerCharacter/src/main/java/sofa/microservice/playerCharacter/PlayerCharacterApplication.java
@@ -0,0 +1,13 @@
+package sofa.microservice.playerCharacter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerCharacterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerCharacterApplication.class, args);
+	}
+
+}
---
+++ playerCharacter/src/test/java/sofa/microservice/playerCharacter/PlayerCharacterApplicationTests.java
@@ -0,0 +1,13 @@
+package sofa.microservice.playerCharacter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerCharacterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
