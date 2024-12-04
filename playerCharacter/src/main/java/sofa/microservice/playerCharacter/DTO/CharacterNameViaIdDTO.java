cf307ba973b128de84036c4d3601ba0fb26f7e62
kyppen <fagerhus.sondre@gmail.com>
2024-12-04 17:48:48 +0100

    idk

--- playerCharacter/src/main/java/sofa/microservice/playerCharacter/DTO/CharacterNameViaIdDTO.java
+++ playerCharacter/src/main/java/sofa/microservice/playerCharacter/DTO/CharacterNameViaIdDTO.java
@@ -1,4 +1,10 @@
 package sofa.microservice.playerCharacter.DTO;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CharacterNameViaIdDTO {
+    private String characterName;
 }
