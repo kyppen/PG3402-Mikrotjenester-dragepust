188da52767b14ffad0ae876ba02ec91ae3e59cc6
Sanderchriss <sanderchriss@gmail.com>
2024-12-11 21:17:05 +0100

    Started consul and docker for userService

--- user/src/main/java/org/api/user/UserService.java
+++ user/src/main/java/org/api/user/UserService.java
@@ -1,13 +1,19 @@
 package org.api.user;
 
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Service;
 @Slf4j
+@ConfigurationProperties(prefix = "servicenames")
 @Service
 public class UserService {
     @Autowired
     private UserRepo userRepo;
+    @Value("${servicenames.userservice}")
+    String userServiceName;
 
     public User saveUser(User user) {
     System.out.println(user);
